3eca399b795906e25b19fbcb54d70073d8b993bf
nicolas-ponce <nicolas.mponce15@gmail.com>
2023-08-17 14:17:07 -0300

    added cart + product section

---
+++ scripts/data.js
@@ -0,0 +1,132 @@
+const plants = [
+    {
+        id: 1,
+        name: "Jacinto",
+        sciName: "Hyacinthus",
+        description: "Magia floral en casa. Colores y fragancia cautivadores. Belleza fácil de cuidar. Eleva tu espacio.",
+        lm: "Luz directa",
+        size: "Chica",
+        price: 2399,
+        image: "../assets/img/plants/jacinto.png"
+    },
+    {
+        id: 2,
+        name: "Rosal",
+        sciName: "Rosa",
+        description: "Belleza que perdura. Elegancia atemporal. Amor en cada pétalo. Transforma tu entorno.",
+        lm: "Luz directa",
+        size: "Grande",
+        price: 4899,
+        image: "../assets/img/plants/rosas.jpg"
+    },
+    {
+        id: 3,
+        name: "Veraneras",
+        sciName: "Bougainvillea",
+        description: "Colores del verano todo el año. Frescura y alegría en cada flor. Naturaleza radiante en tu espacio. Vive el verano siempre.",
+        lm: "Luz directa",
+        size: "Grande",
+        price: 3599,
+        image: "../assets/img/plants/veranera.webp"
+    },
+    {
+        id: 4,
+        name: "Begonia",
+        sciName: "Begonia elatior",
+        description: "Color y encanto en un solo lugar. Elegancia natural que florece. Transforma tu espacio con su belleza única. Vive la belleza de la begonia.",
+        lm: "Semi-sombra",
+        size: "Mediana",
+        price: 2499,
+        image: "../assets/img/plants/begonia.webp"
+    },
+    {
+        id: 5,
+        name: "Azalea",
+        sciName: "Azalea japonica",
+        description: "Estallido de colores en tu entorno. Elegancia efímera que perdura. Transforma tu espacio con su encanto floral. Disfruta de las azaleas.",
+        lm: "Semi-sombra",
+        size: "Mediana",
+        price: 6199,
+        image: "../assets/img/plants/azalea.jpg"
+    },
+    {
+        id: 6,
+        name: "Petunia",
+        sciName: "Petunia hybrida",
+        description: "Paleta de colores vivos que dan vida. Elegancia floral que perdura. Transforma tu espacio con su encanto natural. Sumérgete en la inspiración de las petunias.",
+        lm: "Semi-sombra",
+        size: "Chica",
+        price: 4499,
+        image: "../assets/img/plants/petunia.jpg"
+    },
+    {
+        id: 7,
+        name: "Astilbe",
+        sciName: "Astilbe x arendsii",
+        description: "Plumas de elegancia que perduran. Belleza natural transformadora. Infunde tu espacio con su encanto único. Sumérgete en el mundo de las astilbes.",
+        lm: "Semi-sombra",
+        size: "Mediana",
+        price: 1999,
+        image: "../assets/img/plants/astilbe.jpg"
+    },
+    {
+        id: 8,
+        name: "Jacinto",
+        sciName: "Hyacinthus",
+        description: "",
+        lm: "Luz directa",
+        size: "Chica",
+        price: ,
+        image: "../assets/img/plants/.png"
+    },
+    {
+        id: 9,
+        name: "Jacinto",
+        sciName: "Hyacinthus",
+        description: "",
+        lm: "Luz directa",
+        size: "Chica",
+        price: ,
+        image: "../assets/img/plants/.png"
+    },
+    {
+        id: 10,
+        name: "Jacinto",
+        sciName: "Hyacinthus",
+        description: "",
+        lm: "Luz directa",
+        size: "Chica",
+        price: ,
+        image: "../assets/img/plants/.png"
+    },
+    {
+        id: 11,
+        name: "Jacinto",
+        sciName: "Hyacinthus",
+        description: "",
+        lm: "Luz directa",
+        size: "Chica",
+        price: ,
+        image: "../assets/img/plants/.png"
+    },
+    {
+        id: 12,
+        name: "Jacinto",
+        sciName: "Hyacinthus",
+        description: "",
+        lm: "Luz directa",
+        size: "Chica",
+        price: ,
+        image: "../assets/img/plants/.png"
+    },
+    {
+        id: 13,
+        name: "Jacinto",
+        sciName: "Hyacinthus",
+        description: "",
+        lm: "Luz directa",
+        size: "Chica",
+        price: ,
+        image: "../assets/img/plants/.png"
+    },
+]
